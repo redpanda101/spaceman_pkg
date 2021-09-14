bd84182c6d8979d00326767575daaa95c6c1572f
Eris <redpanda1675@gmail.com>
2021-09-14 19:31:53 +1000

    Added build capability kinda, generates a makefile in project/build, use with the -g for generating command and -b for generating ang building

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,66 @@
+#[macro_use]
+extern crate ini;
+
 mod spaceman_package;
 use spaceman_package::*;
 use std::env;
 
-
-
 #[derive(Copy, Clone)]
-struct Options(bool, bool, bool);
+struct Options(bool, bool, bool, bool, bool);
 
 fn calls(opts: Options, name: String) {
     let mut lang: Lang = Lang::C;
-    if opts.2 { //help flag
+    if opts.4 {
+        //help flag
         help(opts);
         return ();
     }
 
-    if opts.0 { //c++ flag
+    if opts.0 {
+        //c++ flag
         lang = Lang::CPP;
     }
-    
-    if opts.1 { //create flag
+
+    if opts.1 {
+        //create flag
         SpacemanPackage::new(name, lang).create();
+    }
+
+    else if opts.2 {
+        SpacemanPackage::new(name, lang).create_makefiles();
+    }
+
+    else if opts.3 {
+        SpacemanPackage::new(name, lang).build();
     }
 }
 
-fn help(opts: Options){
+fn help(opts: Options) {
     if opts.0 {
         println!("Flag switches the default language");
-    }
-    else if opts.1 {
+    } else if opts.1 {
         println!("Flag creates a new project");
-    }
-    else if opts.2 {
+    } else if opts.4 {
         println!("Spaceman Package Manager:\n-n | new | --new : creates a new project \n-cpp | --lang:cpp : changes language to c++\n-h | help | --help : prints this message");
     }
 }
 
 fn main() {
-    let mut opts = Options(false, false, false); //sets all flags off
+    let mut opts = Options(false, false, false, false, false); //sets all flags off
     let mut name: String = String::new();
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 1 {
-        opts.2 = true;
+        opts.4 = true;
     }
 
     for i in 1..args.len() {
         match args[i].as_str() {
             "-n" | "new" | "--new" => opts.1 = true,
             "-cpp" | "--lang:cpp" => opts.0 = true,
-            "-h" | "help" | "--help" => opts.2 = true,
+            "-h" | "help" | "--help" => opts.4 = true,
+            "-b" | "build" | "--build" => opts.3 = true,
+            "-g" | "gen" | "--genetate" => opts.2 = true,
             _ => name = args[i].clone(),
         }
     }
--- src/spaceman_package.rs
+++ src/spaceman_package.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::ops::Add; //for string add
-//use simple_config_parser::config::Config; //for build, will implement later
+use std::process::Command;
 
 ///Language enum
 pub enum Lang {
@@ -25,6 +24,14 @@ impl SpacemanPackage {
         }
     }
 
+    fn get_file_ext(&self) -> String {
+        if let Lang::C = self.language {
+            ".c".to_string()
+        } else {
+            ".cc".to_string()
+        }
+    }
+
     //File Structure of created package
     // (Package Name)
     //      - build
@@ -32,32 +39,80 @@ impl SpacemanPackage {
     //          - main.c / main.cc
     //      - spaceman.config
 
-
     /// ### `create(&self)`
     /// creates package on file system
 
     pub fn create(&self) {
         //file templates
-        let hello_world_c = "#include <stdio.h>\nint main() {\n    printf(\"hello world\");\n}\0";
-        let config_file = format!("name = \"{}\"\ncompiler = \"gnu\"\nmakefile_type = \"gnu\"", self.name);
+        let hello_world_c = "#include <stdio.h>\nint main() {\n    printf(\"hello world\");\n}";
+        let config_file = format!(
+            "[spaceman_package]\nname={}\ncompiler=gnu\ndeps=\nlanguage={}",
+            self.name,
+            self.get_file_ext()
+        );
 
         //create directories
         fs::create_dir(self.name.as_str()).expect("Could not create dir: "); //create project dir
         fs::create_dir(format!("{}/src", self.name).as_str()).expect("Could not create dir: "); //create source dir
         fs::create_dir(format!("{}/build", self.name).as_str()).expect("Could not create dir: "); //create build dir
 
-        //if language is c, write main.c
-        if let Lang::C = self.language {
-            fs::write(format!("{}/src/main.c", self.name), hello_world_c).expect("Could not write to main: "); //write to main
-        } 
-
-        //else if language is c++, write main.cc
-        else {
-            fs::write(format!("{}/src/main.cc", self.name), hello_world_c).expect("Could not write to main: "); //write to main
-        }
-        fs::write(format!("{}/spaceman.config", self.name), config_file).expect("Could not write to config: ");
+        fs::write(
+            format!("{}/src/main{}", self.name, self.get_file_ext()),
+            hello_world_c,
+        )
+        .expect("Could not write to main: ");
+        fs::write(format!("{}/spaceman.ini", self.name), config_file)
+            .expect("Could not write to config: ");
     }
-    pub fn build(&self){
-        //TODO: add build
+
+    pub fn create_makefiles(&self) {
+        let config = ini!(format!("{}/spaceman.ini", self.name).as_str());
+        let s_config = config.get("spaceman_package").unwrap();
+        let compiler = s_config.get("compiler").unwrap().as_ref().unwrap();
+        //let deps = config.get("deps").unwrap();
+        let lang = s_config.get("language").unwrap().as_ref().unwrap();
+
+        let compiler_cmd = match compiler.as_str() {
+            "gcc" | "g++" | "gnu" => {
+                if lang == ".c" {
+                    "gcc"
+                } else {
+                    "g++"
+                }
+            }
+            "clang" | "clang++" | "llvm" => {
+                if lang == ".c" {
+                    "clang"
+                } else {
+                    "clang++"
+                }
+            }
+            _ => "gcc",
+        };
+
+        //write to makefile
+        fs::write(
+            format!("{}/build/Makefile", self.name),
+            format!(//contents of makefile
+                "debug:\n\t{} -o {} -c ../src/main{}\nrelease:\n\t{} -o {} -c ../src/main{}\ninstall:\n\tmake release\n\tcp build/{} /usr/bin/{}",
+                compiler_cmd,
+                self.name,
+                self.get_file_ext(),
+                compiler_cmd, //copied again for release
+                self.name,
+                self.get_file_ext(),
+                self.name, self.name
+            ),
+        )
+        .expect("Could not write Makefile: ");
+    }
+
+    pub fn build(&self) {
+        self.create_makefiles();
+        Command::new("make")
+            .arg("debug")
+            .arg(format!("--directory={}/build", self.name))
+            .spawn()
+            .expect("could not build program: ");
     }
 }
